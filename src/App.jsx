183d793bd53cb70301909d741fcb5edd65feb7ec
Aisha Keita <aishacassel@gmail.com>
2024-07-07 19:24:48 +0200

    Modify App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import CountriesAppBar from "./CountriesAppBar"
-import CountriesFlags from "./CountriesFlags"
+import CountriesAppBar from './CountriesAppBar'
+import CountriesFlags from './CountriesFlags'
+import AllCountries from './AllCountries'
+import FlagButton from './FlagButton'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-
-
-  return (
-    <>
-      <CountriesAppBar />
-      <CountriesFlags/>
-
-    </>
-  )
+    return (
+        <div className="App">
+            <>
+                <CountriesAppBar />
+            </>
+            <Routes>
+                <Route path="/" element={<CountriesFlags />} />
+                <Route path="/flags" element={<FlagButton />} />
+                <Route path="/allCountries" element={<AllCountries />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
